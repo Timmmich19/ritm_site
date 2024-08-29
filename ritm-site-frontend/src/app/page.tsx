bb9be3a2ee8c8c9a4882c9177762c99afddccc96
Timmmich <tg.akhmatov@gmail.com>
2024-08-29 21:57:38 +0500

    added styles for main wrapper

--- ritm-site-frontend/src/app/page.tsx
+++ ritm-site-frontend/src/app/page.tsx
@@ -1,8 +1,8 @@
 import Welcome from "./widgets/welcome";
-
+import "@/app/globals.css";
 export default function Home() {
   return (
-    <main>
+    <main className="main_wrapper">
       <Welcome />
     </main>
   );
