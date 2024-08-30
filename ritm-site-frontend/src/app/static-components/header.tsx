fbd423654cbe5a65e0b2351c75f1c7c31a4bf09c
slomec <slomeec@gmail.com>
2024-08-30 20:34:40 +0500

    added flex in header and svg icon

--- ritm-site-frontend/src/app/static-components/header.tsx
+++ ritm-site-frontend/src/app/static-components/header.tsx
@@ -1,7 +1,10 @@
+import Image from "next/image";
+
 export default function Header() {
   return (
-    <header>
-      <h1>RITM</h1>
+    <header className="flex gap-4">
+      <Image src="/logo.svg" alt="RITM logo" width={48} height={48} />
+      <h1 className="header-element">RITM</h1>
     </header>
   );
 }
