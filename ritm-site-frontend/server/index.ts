de9a3b247f4df0137cabcfb830e9ade4b20a5d4b
Timmmich <tg.akhmatov@gmail.com>
2024-09-08 21:44:18 +0500

    added dotenv config and gitingore for it

--- ritm-site-frontend/server/index.ts
+++ ritm-site-frontend/server/index.ts
@@ -2,7 +2,7 @@ import express, { Express, Request, Response } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 
-dotenv.config();
+dotenv.config({ path: "./server/.env" });
 
 const app: Express = express();
 const port = process.env.PORT || 8000;
