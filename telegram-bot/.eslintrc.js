05816d928da2d86388eafb722d232b81ec8c759b
Timmmich <tg.akhmatov@gmail.com>
2024-09-10 00:19:01 +0500

    init telegram bot

---
+++ telegram-bot/.eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    node: true,
+  },
+  extends: ["plugin:@typescript-eslint/recommended"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module",
+  },
+  rules: {
+    // Добавьте свои правила здесь
+  },
+};
